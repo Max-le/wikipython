bfc6db72e780b66ca5dc0e4ddbe1e4f1ddc32066
Max <max.09@outlook.com>
2019-05-21 17:52:14 +0200

    merge the two components

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -6,8 +6,7 @@ function App() {
 
     return (
     <div className="App">
-      <InputWord/>
-      <Translator/>
+      <Translator word={"Love"}/>
 
 
     </div>
--- client/my-app/src/components/Translator.js
+++ client/my-app/src/components/Translator.js
@@ -1,9 +1,20 @@
 import React, { Component } from "react";
+import InputWord from "./InputWord"
 class Translator extends Component {
   constructor(props) {
     super(props);
     this.state = {word: ''};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
   }
+  handleSubmit(event){
+    alert("Received : "+this.state.inputWord)
+}
+handleChange(event){
+    this.setState({inputWord: event.target.value});
+}
+
   componentDidMount() {
     fetch("http://localhost:5000/translate?word=complexity")
     .then((res) => {return res.json()}).then(((data) => this.setState({word: data})))
@@ -21,12 +32,22 @@ class Translator extends Component {
       <div>
         <h1>{myWord[0].translation}</h1>
         <h2>{aKey}</h2>
+        <h3>Props : {this.props}</h3>
 
       </div>
     );
     }
     else return (
-      <p id='results'>Waiting for results...</p>
+      <div>
+            <div>
+            <span>Enter the word to translate here : </span>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" value={this.state.value} onChange={this.handleChange} />
+                    <input type="submit" value="Submit"/>
+                </form>
+            </div>
+            );      
+      </div>
     )
   }
 }
