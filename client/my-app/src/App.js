63a244ae36f6ec0e8993110cf43bc4f10576574f
Max <max.09@outlook.com>
2019-05-21 16:09:46 +0200

    add an input form to type a word to translate

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
 import Translator from './components/Translator'
+import InputWord from './components/InputWord'
 function App() {
-  
-  return (
+
+    return (
     <div className="App">
+      <InputWord/>
       <Translator/>
+
+
     </div>
   );
 }
-
 export default App;
--- client/my-app/src/components/Translator.js
+++ client/my-app/src/components/Translator.js
@@ -1,28 +1,35 @@
 import React, { Component } from "react";
-
 class Translator extends Component {
   constructor(props) {
     super(props);
     this.state = {word: ''};
   }
   componentDidMount() {
-    fetch("http://localhost:5000/translate?word=hello")
+    fetch("http://localhost:5000/translate?word=complexity")
     .then((res) => {return res.json()}).then(((data) => this.setState({word: data})))
 
     }
   render() {
-
- 
-    Object.keys(this.state.word).forEach(aKey => {
-      console.log("KEY : "+aKey)
-      console.log(this.state.word[aKey])
-    });
-    console.log(this.state.word)
+    if (this.state.word !== undefined && this.state.word !== ""){
+      var aKey = Object.keys(this.state.word)[0]
+      console.log("WORD : "+this.state.word)
+      console.log("KEY : "+aKey)  
+      var myWord = this.state.word[aKey]
+      console.log(myWord[0].translation)
 
     return (
-        <p>Translator</p>
+      <div>
+        <h1>{myWord[0].translation}</h1>
+        <h2>{aKey}</h2>
+
+      </div>
     );
+    }
+    else return (
+      <p id='results'>Waiting for results...</p>
+    )
   }
 }
+ 
 export default Translator;
 ;
