659c31f42e12edfb9607c4d72fa1ee409ea87b7c
Max <max.09@outlook.com>
2019-05-24 17:33:38 +0200

    fill by a random word at loadinfg of the component

--- client/my-app/src/components/Translator.js
+++ client/my-app/src/components/Translator.js
@@ -1,11 +1,21 @@
 import React, { Component } from "react";
+
 class Translator extends Component { 
+
   constructor(props) {
     super(props);
     this.state = {wordInput: "query", word_data:[], targetLang: "German", requestCompleted: false};
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this); 
     this.handleLanguageChange = this.handleLanguageChange.bind(this);
+  }
+
+  changeToRandomWord(){
+    var randomWords = require('random-words');
+    this.setState({wordInput:randomWords()})
+  }
+  componentDidMount(){
+    this.changeToRandomWord()
   }
   handleSubmit(event){
     //TO-DO :  CHANGE REDUX STATE
@@ -42,32 +52,50 @@ class Translator extends Component {
         <option value="French">French</option>
         <option value="Dutch">Dutch</option>
         <option value="Italian">Italian</option>
+        <option value="Swedish">Swedish</option>
 
       </select>
       
       <input type="submit" value="Submit"/>
       </form>
-      
       </div>)
+    }
+    credits(){
+      return(
+      <div >
+        <p>--------------------</p>
+        <em>Data from Wiktionary.org</em><br></br>
+        <img alt='logo' width="100" height="100" src='https://upload.wikimedia.org/wikipedia/meta/6/61/Wiktionary_propsed-smurrayinchester.png'/>
+      </div>
+      )
     }
     render() {
       if (this.state.requestCompleted === true ){
         
         console.log(this.state.wordInput)
         const keys = Object.keys(this.state.word_data)
+
+        if ( keys === undefined || keys.length === 0) {
+          console.log("no result found.")
+          return (
+            <div>
+            {this.wordInput()}
+             <p>No translation found.</p>
+             </div>)
+        }
+        else{
+        }
         console.log(keys)
-        console.log(this.state.word_data[keys[0]])
-        keys.forEach(key => { console.log(this.state.word_data[key][0].translation)
-          
+        keys.forEach(key => { console.log(this.state.word_data[key][0].translation)       
         });
         const listKeys = keys.map((key, index) => 
         <p key={index}>{key} : <i>{this.state.word_data[key][0].translation}</i> ({this.state.word_data[key][0].gender})</p>)
         return ( 
-          
-          <div>{this.wordInput()}{listKeys}</div>)
+          <div>{this.wordInput()}{listKeys}{this.credits()}</div>)
         }
         else return (
           this.wordInput()
+          
           );
         }
         
